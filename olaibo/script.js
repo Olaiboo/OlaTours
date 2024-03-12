12671479b14ae4ae34fb3f603f5a1590e26a09ce
Olaiboo <161310838+Olaiboo@users.noreply.github.com>
2024-03-12 14:31:54 +0100

    Add files via upload

---
+++ olaibo/script.js
@@ -0,0 +1,44 @@
+
+let searchBtn = document.querySelector('#search-btn');
+let searchBar = document.querySelector('.search-bar-container');
+let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.login-form-container');
+let formClose = document.querySelector('#form-close');
+
+let videoBtn = document.querySelectorAll('.vid-btn');
+
+window.onscroll = () => {
+searchBtn.classList.remove('fa-times');
+searchBtn.classList.remove('active');
+};
+
+searchBtn.addEventListener('click', () =>{
+searchBtn.classList.toggle('fa-times');
+searchBar.classList.toggle('active');
+});
+
+formBtn.addEventListener('click', () =>{
+loginForm.classList.add('active');
+});
+
+formClose.addEventListener('click', () =>{
+loginForm.classList.remove('active');
+});
+
+videoBtn.forEach(btn =>{
+    btn.addEventListener('click', ()=>{
+    document.querySelector('.controls .active').classList.remove('active');
+    btn.classList.add('active');
+    let src = btn.getAttribute('data-src');
+    document.querySelector('#video-slider').src = src;
+    });
+});
+
+var swiper = new swiper(".review-slider", {
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    }
+});
